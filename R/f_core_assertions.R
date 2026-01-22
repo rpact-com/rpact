1d8fc1a6493903816e990d80c6000eb542fb4289
Daniel Sabanes Bove <daniel@rconis.com>
2026-01-22 14:42:35 +0800

    fix assertion condition to allow for vector `lambda2` (which led to an error previously)

--- R/f_core_assertions.R
+++ R/f_core_assertions.R
@@ -3452,7 +3452,7 @@ NULL
         lambda2 <- lambda1 / theta
     }
     
-    if (sampleSizeEnabled && any(!is.na(lambda1)) && !is.na(lambda2) && !is.na(thetaH0) && 
+    if (sampleSizeEnabled && !all(is.na(lambda1)) && !all(is.na(lambda2)) && !is.na(thetaH0) && 
             any(abs(lambda1 / lambda2 - thetaH0) < 1e-12, na.rm = TRUE)) {
         stop(
             C_EXCEPTION_TYPE_CONFLICTING_ARGUMENTS,
